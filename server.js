303005e509badcc6cf252c01547972f8ac51f537
ogheneruno <orunosaduwa@gmail.com>
2019-10-29 13:44:13 +0100

    initial commit

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const chalk = require('chalk');
 const bodyParser = require('body-parser');
 const app = express();
 
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+
 
 const PORT = process.env.PORT || 8080;
 
@@ -17,6 +21,21 @@ app.use(bodyParser.json())
 
 
 app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/sendgrid', (req, res) => {
+
+   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'orunosaduwa@gmail.com',
+  from: 'node@app.com',
+  subject: 'Letter of Acknowledgemnt',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>SendGrid is awesome</strong>',
+};
+sgMail.send(msg);
+res.send('message sent');
+
+})
 
 
 
